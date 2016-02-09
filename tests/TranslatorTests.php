6621f6dd066045938a09963e5f424bad4ca506db
Will <will.skates@ntlworld.com>
2016-02-09 18:36:54 +0000

    Reworking the tests to use setUp() rather than running a make() method each time.

--- tests/TranslatorTests.php
+++ tests/TranslatorTests.php
@@ -5,73 +5,72 @@ use WillSkates\Translator\Translator;
 class TranslatorTests extends PHPUnit_Framework_TestCase
 {
 
-	public function make()
+	private $this->translator;
+
+	public function setUp()
 	{
-		return new Translator();
+		$this->translator = new Translator();
 	}
 
 	public function testCreation()
 	{
-	    $translator = $this->make();
-	    $this->assertEquals('en', $translator->getDefaultLang());
+	    $this->assertEquals('en', $this->translator->getDefaultLang());
 	}
 
 	public function testEnglish()
 	{
-
-		$translator = $this->make();
 
 		$en = array(
 			'hello' => 'hi',
 			'nice' => 'yay'
 		);
 
-		$translator->setTranslations($en, 'en');
+		$this->translator->setTranslations($en, 'en');
 
-		$translated = $translator->getTranslations('en');
+		$translated = $this->translator->getTranslations('en');
 
 		$this->assertCount(count($en), $translated);
 
-		$translator->setDefaultLang('en');
+		$this->translator->setDefaultLang('en');
 
 		foreach ( $en as $orig => $translation ) {
 			$this->assertTrue(isset($translated[$orig]));
 			$this->assertEquals($translation, $translated[$orig]);
-			$translator->setTranslation($orig, $translation, 'en');
-			$this->assertEquals($translation, $translator->getTranslation($orig, 'en'));
-			$this->assertEquals($translation, $translator->getTranslation($orig));
+			$this->translator->setTranslation($orig, $translation, 'en');
+			$this->assertEquals($translation, $this->translator->getTranslation($orig, 'en'));
+			$this->assertEquals($translation, $this->translator->getTranslation($orig));
 		}
 
 		//test merge.
 		$en['hello'] = 'hey';
 		$en['person'] = 'person';
-		$translator->setTranslations($en, 'en', true);
+		$this->translator->setTranslations($en, 'en', true);
 
-		$translated = $translator->getTranslations('en');
+		$translated = $this->translator->getTranslations('en');
 
 		$this->assertCount(count($en), $translated);
 
 		foreach ( $en as $orig => $translation ) {
 			$this->assertTrue(isset($translated[$orig]));
 			$this->assertEquals($translation, $translated[$orig]);
-			$translator->setTranslation($orig, $translation, 'en');
-			$this->assertEquals($translation, $translator->getTranslation($orig, 'en'));
-			$this->assertEquals($translation, $translator->getTranslation($orig));
+			$this->translator->setTranslation($orig, $translation, 'en');
+			$this->assertEquals($translation, $this->translator->getTranslation($orig, 'en'));
+			$this->assertEquals($translation, $this->translator->getTranslation($orig));
 		}
 
 		//test merge.
 		unset($en['nice']);
 		$en['hello'] = 'hey';
 		$en['person'] = 'person';
-		$translator->setTranslations($en, 'en', false);
+		$this->translator->setTranslations($en, 'en', false);
 
-		$translated = $translator->getTranslations('en');
+		$translated = $this->translator->getTranslations('en');
 
 		$this->assertCount(count($en), $translated);
 
 		foreach ( $translated as $orig => $translation ) {
 			$this->assertTrue(isset($en[$orig]));
-			$this->assertEquals($translation, $translator->getTranslation($orig, 'en'));
+			$this->assertEquals($translation, $this->translator->getTranslation($orig, 'en'));
 		}
 
 	}
@@ -79,59 +78,59 @@ class TranslatorTests extends PHPUnit_Framework_TestCase
 	public function testGerman()
 	{
 
-		$translator = $this->make();
+		$this->translator = $this->make();
 
 		$de = array(
 			'hello' => 'hallo',
 			'nice' => 'gut'
 		);
 
-		$translator->setTranslations($de, 'de');
+		$this->translator->setTranslations($de, 'de');
 
-		$translated = $translator->getTranslations('de');
+		$translated = $this->translator->getTranslations('de');
 
 		$this->assertCount(count($de), $translated);
 
-		$translator->setDefaultLang('de');
+		$this->translator->setDefaultLang('de');
 
 		foreach ( $de as $orig => $translation ) {
 			$this->assertTrue(isset($translated[$orig]));
 			$this->assertEquals($translation, $translated[$orig]);
-			$translator->setTranslation($orig, $translation, 'de');
-			$this->assertEquals($translation, $translator->getTranslation($orig, 'de'));
-			$this->assertEquals($translation, $translator->getTranslation($orig));
+			$this->translator->setTranslation($orig, $translation, 'de');
+			$this->assertEquals($translation, $this->translator->getTranslation($orig, 'de'));
+			$this->assertEquals($translation, $this->translator->getTranslation($orig));
 		}
 
 		//test merge.
 		$de['hello'] = 'hey';
 		$de['person'] = 'person';
-		$translator->setTranslations($de, 'de', true);
+		$this->translator->setTranslations($de, 'de', true);
 
-		$translated = $translator->getTranslations('de');
+		$translated = $this->translator->getTranslations('de');
 
 		$this->assertCount(count($de), $translated);
 
 		foreach ( $de as $orig => $translation ) {
 			$this->assertTrue(isset($translated[$orig]));
 			$this->assertEquals($translation, $translated[$orig]);
-			$translator->setTranslation($orig, $translation, 'de');
-			$this->assertEquals($translation, $translator->getTranslation($orig, 'de'));
-			$this->assertEquals($translation, $translator->getTranslation($orig));
+			$this->translator->setTranslation($orig, $translation, 'de');
+			$this->assertEquals($translation, $this->translator->getTranslation($orig, 'de'));
+			$this->assertEquals($translation, $this->translator->getTranslation($orig));
 		}
 
 		//test merge.
 		unset($de['nice']);
 		$de['hello'] = 'hey';
 		$de['person'] = 'person';
-		$translator->setTranslations($de, 'de', false);
+		$this->translator->setTranslations($de, 'de', false);
 
-		$translated = $translator->getTranslations('de');
+		$translated = $this->translator->getTranslations('de');
 
 		$this->assertCount(count($de), $translated);
 
 		foreach ( $translated as $orig => $translation ) {
 			$this->assertTrue(isset($de[$orig]));
-			$this->assertEquals($translation, $translator->getTranslation($orig, 'de'));
+			$this->assertEquals($translation, $this->translator->getTranslation($orig, 'de'));
 		}
 
 	}
@@ -139,59 +138,59 @@ class TranslatorTests extends PHPUnit_Framework_TestCase
 	public function testBro()
 	{
 
-		$translator = $this->make();
+		$this->translator = $this->make();
 
 		$bro = array(
 			'hello' => 'Yo!',
 			'nice' => 'Dayum!'
 		);
 
-		$translator->setTranslations($bro, 'bro');
+		$this->translator->setTranslations($bro, 'bro');
 
-		$translated = $translator->getTranslations('bro');
+		$translated = $this->translator->getTranslations('bro');
 
 		$this->assertCount(count($bro), $translated);
 
-		$translator->setDefaultLang('bro');
+		$this->translator->setDefaultLang('bro');
 
 		foreach ( $bro as $orig => $translation ) {
 			$this->assertTrue(isset($translated[$orig]));
 			$this->assertEquals($translation, $translated[$orig]);
-			$translator->setTranslation($orig, $translation, 'bro');
-			$this->assertEquals($translation, $translator->getTranslation($orig, 'bro'));
-			$this->assertEquals($translation, $translator->getTranslation($orig));
+			$this->translator->setTranslation($orig, $translation, 'bro');
+			$this->assertEquals($translation, $this->translator->getTranslation($orig, 'bro'));
+			$this->assertEquals($translation, $this->translator->getTranslation($orig));
 		}
 
 		//test merge.
 		$bro['hello'] = 'hey';
 		$bro['person'] = 'person';
-		$translator->setTranslations($bro, 'bro', true);
+		$this->translator->setTranslations($bro, 'bro', true);
 
-		$translated = $translator->getTranslations('bro');
+		$translated = $this->translator->getTranslations('bro');
 
 		$this->assertCount(count($bro), $translated);
 
 		foreach ( $bro as $orig => $translation ) {
 			$this->assertTrue(isset($translated[$orig]));
 			$this->assertEquals($translation, $translated[$orig]);
-			$translator->setTranslation($orig, $translation, 'bro');
-			$this->assertEquals($translation, $translator->getTranslation($orig, 'bro'));
-			$this->assertEquals($translation, $translator->getTranslation($orig));
+			$this->translator->setTranslation($orig, $translation, 'bro');
+			$this->assertEquals($translation, $this->translator->getTranslation($orig, 'bro'));
+			$this->assertEquals($translation, $this->translator->getTranslation($orig));
 		}
 
 		//test merge.
 		unset($bro['nice']);
 		$bro['hello'] = 'hey';
 		$bro['person'] = 'person';
-		$translator->setTranslations($bro, 'bro', false);
+		$this->translator->setTranslations($bro, 'bro', false);
 
-		$translated = $translator->getTranslations('bro');
+		$translated = $this->translator->getTranslations('bro');
 
 		$this->assertCount(count($bro), $translated);
 
 		foreach ( $translated as $orig => $translation ) {
 			$this->assertTrue(isset($bro[$orig]));
-			$this->assertEquals($translation, $translator->getTranslation($orig, 'bro'));
+			$this->assertEquals($translation, $this->translator->getTranslation($orig, 'bro'));
 		}
 
 	}
@@ -199,10 +198,10 @@ class TranslatorTests extends PHPUnit_Framework_TestCase
 	public function testTranslator()
 	{
 
-		$translator = $this->make();
+		$this->translator = $this->make();
 
-		$translator->setDefaultLang('de');
-		$this->assertEquals('de', $translator->getDefaultLang());
+		$this->translator->setDefaultLang('de');
+		$this->assertEquals('de', $this->translator->getDefaultLang());
 
 	}
 
